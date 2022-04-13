e69562f4c714d493167ab6b40bc9a9c1f95bb81d
Diana <diana45637@live.com>
2022-04-13 18:48:59 -0300

    feat: disables the select when the same time is chosen twice

--- agendamento-frontend/src/pages/Scheduling/index.js
+++ agendamento-frontend/src/pages/Scheduling/index.js
@@ -5,18 +5,30 @@ import { Calendar, Clock } from 'tabler-icons-react';
 import { useState } from "react";
 
 
-const SchedulingrForm = ({setForm, form}) => {
+const voucher = [];
+const time = Array(20).fill(0).map((_, index) => {
+   return {name: `${index+4}:00h`, disabled: false, count: 0}
+});
+
+const SchedulingrForm = ({form, setForm}) => {
 
     const date = new Date();
     //const [schedulingDate, setSchedulingDate] = useState('');
 
-    const time = Array(20).fill(0).map((_, index) => {
-       return {name: `${index+4}:00h`, disabled: true}
-    });
     
     // const disabled = Array(20).fill(0).map(() => false);
     // disabled[1] = true;
 
+    const onChange = (event) => {
+        const {
+        target: { name, value },
+        } = event;
+
+        setForm((prevForm) => ({
+        ...prevForm,
+        [name]: value,
+        }));
+    };
     
     return (
     <>
@@ -30,10 +42,14 @@ const SchedulingrForm = ({setForm, form}) => {
         <Input
             id="name"
             name="name"
+            value={form.name}
+            onChange={onChange}
         />
         </InputWrapper>
 
-        <DatePicker 
+        <DatePicker
+            value={form.birthDate}
+            onChange={(value) => onChange({target: {name: "birthDate", value}})}
             mb={8}
             required
             placeholder="Select Birthdate"
@@ -42,6 +58,8 @@ const SchedulingrForm = ({setForm, form}) => {
         />
 
         <DatePicker
+            value={form.schedulingDate}
+            onChange={(value) => onChange({target: {name: "schedulingDate", value}})}
             mb={8}
             required
             placeholder="Select Date"
@@ -51,6 +69,8 @@ const SchedulingrForm = ({setForm, form}) => {
         />
 
 <       Select
+            value={form.schedulingTime}
+            onChange={(value) => onChange({ target: { name: "schedulingTime", value } })}
             required
             label="Choose the time of scheduling"
             placeholder="Time"
@@ -92,11 +112,28 @@ const Scheduling = () => {
        name: '',
        birthDate: new Date(),
        schedulingDate: new Date(),
-       schedulingTime: new Date(),
+       schedulingTime: new Date().getHours(),
     });
 
 
-    const onSubmit = async () => {
+    const onSubmit = async (form) => {
+
+        const scheduling = {
+            ...form,
+            birthDate: form.birthDate.toISOString(),
+            schedulingDate: form.schedulingDate.toISOString(),
+        };
+
+        voucher.push(scheduling); //passo o objeto com as informações do formulario para um array
+
+        const selected = time.findIndex((select) => select.name === voucher[voucher.length-1].schedulingTime); //encontro o index do select escolhido
+
+        if (time[selected].count === 1){
+            time[selected].disabled = true;
+        }
+        
+        time[selected].count++;
+
 
     };
 
@@ -108,7 +145,7 @@ const Scheduling = () => {
 
             <SchedulingrForm setForm={setForm} form={form} />
             
-            <Button onClick={onSubmit}
+            <Button onClick={() => onSubmit(form)}
                     fullWidth
                     variant="gradient" 
                     gradient={{ from: 'indigo', to: 'cyan' }}
